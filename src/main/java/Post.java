d5ba569e5ffbbba2c2a9df5d1cd4eae8b47b34c9
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-03-04 21:55:25 +0300

    solution

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+    public FormDate birthday; // поле для даты рождения
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для согласия
+
+
+}
